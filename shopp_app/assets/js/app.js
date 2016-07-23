96d615ec2921ec58450b4fbc4a3ce65d95422091
thanarino <jaarino@up.edu.ph>
2016-07-23 18:12:43 +0800

    implemented routes. cart still nonexistent

--- shopp_app/assets/js/app.js
+++ shopp_app/assets/js/app.js
@@ -1,10 +1,32 @@
 (function(){
-  var app = angular.module("takeShop", []);
+  var app = angular.module("takeShop", ["ngRoute"]);
+
+  app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/mainPage', {
+        templateUrl: 'pages/shop.html',
+        controller: 'allShopController'
+      })
+      .when('/myCart', {
+        templateURL: 'pages/cart.html',
+        controller: 'myCartController'
+      })
+      .otherwise({
+        redirectTo: '/mainPage'
+      });
+  }]);
 
   app.controller('allShopController', ['$scope', '$http', function($scope, $http){
     $http.get('http://acssuplb.github.io/json/exer1_inventory.json')
          .success(function(data){
             $scope.products = data;
          });
+  }]);
+
+  app.controller('myCartController', ['$scope', '$http', function($scope, $http){
+    $http.get('../assets/json/cart.json')
+      .success(function(data){
+        $scope.items = data;
+      });
   }]);
 })();
